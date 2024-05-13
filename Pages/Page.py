da88d3dcf215befd3269ecf9feaefa6e7f7eb203
JarrColl <jarronc@protonmail.com>
2024-05-13 14:53:47 +1000

    page abstract class

--- Pages/Page.py
+++ Pages/Page.py
@@ -1,3 +1,11 @@
+from abc import abstractmethod
 
-class Page():
-    
+
+class Page:
+    """
+    The Page interface declares display operations so that child classes can be passed to a common function when moving through the interface.
+    """
+
+    @abstractmethod
+    def display(self):
+        pass
