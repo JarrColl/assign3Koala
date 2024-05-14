14dda3593aa755b3c9b544f8d6cf2a473b71e808
JarrColl <jarronc@protonmail.com>
2024-05-14 16:27:41 +1000

    login manager more stuff

--- Managers/LoginManager.py
+++ Managers/LoginManager.py
@@ -5,10 +5,20 @@ from ..MainClasses.Staff import Staff
 class LoginManager:
 
     def __init__(self, db: DatabaseConnection):
+        self.staff = None
         self.db: DatabaseConnection = db
 
-    def login(username: str, password: str):
-        pass
+    def login(self, username: str, password: str):
+        staff_list = self.db.getTableData("staff")
+        # TODO: When the staff object is created, use the read in data to create the staff object and store it.
+        next(
+            (
+                staff
+                for staff in staff_list
+                if (staff["username"] == username and staff["password"] == password)
+            ),
+            None,
+        )
 
-    def checkAccess(staff: Staff, permission: str):
+    def checkAccess(self, staff: Staff, permission: str):
         pass
