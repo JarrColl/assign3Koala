7b1b786e698090036d263e39dfe840d309f871cf
RyanCorreia7 <correiaryan30@gmail.com>
2024-05-30 11:37:23 +1000

    StaffPage.py

--- Pages/StaffPage.py
+++ Pages/StaffPage.py
@@ -1,3 +1,32 @@
 # !!! Only accessed by manager role.
 # List all staff information
+from DatabaseConnection import DatabaseConnection
+from Managers.LoginManager import LoginManager
+
+db = DatabaseConnection()
+
+class StaffPage:
+    @staticmethod
+    def display(login_manager: LoginManager):
+        if not login_manager.staff or login_manager.staff.role.name != "Manager":
+            print("Access denied. Only managers can access this page.")
+            return
+
+        staff_data = db.getTableData("staff")
+        StaffPage.print_staff_info(staff_data)
+
+    @staticmethod
+    def print_staff_info(staff_data):
+        print("----------Staff Information----------")
+
+        header = "| {:<5} | {:<20} | {:<20} | {:<10} |".format("ID", "Username", "Name", "Role")
+        print("-" * len(header))
+        print(header)
+        print("-" * len(header))
+
+        for staff in staff_data:
+            role = "Manager" if staff['role'] == 0 else "Staff"
+            print("| {:<5} | {:<20} | {:<20} | {:<10} |".format(staff['id'], staff['username'], staff['name'], role))
+
+        print("-" * len(header))
 
